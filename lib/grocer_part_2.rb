18e310c4ad4e600ee821ef097d476c52f8e9498a
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-07 01:35:15 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,20 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each_with_object([]) do |product, final_cart|
+    coupons.each do |coupon|
+      if product[:item] == coupon[:item] && product[:count] >= coupon[:num]
+        product[:count] = product[:count] - coupon[:num]
+        final_cart << product
+        discounted_product = {item: "#{product[:item]} W/COUPON", price: coupon[:cost] / coupon[:num], clearance: true, count: coupon[:num]}
+        final_cart << discounted_product
+      end
+    end
+    item_found = final_cart.find { |final_product| final_product[:item] == product[:item] }
+    if !item_found
+      final_cart << product
+    end
+  end
 end
 
 def apply_clearance(cart)
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -1,34 +1,19 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
-  index = 0
-
-  collection.each do |grocery_item|
-    return grocery_item if grocery_item[:item] === name 
-    index += 1
+  collection.find do |product|
+    product[:item] == name
   end
-
-  nil
 end
 
-
 def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-  
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
+  cart.each_with_object([]) do |product, final_cart|
+    new_item = find_item_by_name_in_collection(product[:item], final_cart)
+    if !new_item
+      product[:count] = 1
+      final_cart << product
     else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
+      new_item[:count] += 1
     end
-    index += 1
   end
-  new_cart
 end
